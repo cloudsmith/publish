c2e5b439b5cb5bf967c0dfaddc09749d22a23e41
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-07-12 04:25:28 +0000

    Cleanup of Engine API and running commands. Added Handlers to be able to do more interesting things in the UI.
    git-svn-id: http://svn.cloudsmith.com/repos/com.cloudsmith.publish/trunk@5609 9a51ea61-df1d-0410-9560-c96666059826

--- com.cloudsmith.publish.ui/src/com/cloudsmith/publish/ui/commands/ExecuteHandler.java
+++ com.cloudsmith.publish.ui/src/com/cloudsmith/publish/ui/commands/ExecuteHandler.java
@@ -1,18 +1,16 @@
 package com.cloudsmith.publish.ui.commands;
 
 import java.io.PrintStream;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.b3.backend.core.B3BackendException;
+import org.eclipse.b3.backend.core.IResultStatus;
 import org.eclipse.b3.backend.evaluator.b3backend.BExpression;
-import org.eclipse.b3.backend.evaluator.b3backend.IFunction;
 import org.eclipse.b3.beelang.ui.BeeLangConsoleUtils;
 import org.eclipse.b3.build.BeeModel;
-import org.eclipse.b3.build.core.B3BuildConstants;
 import org.eclipse.b3.build.core.B3BuildEngine;
-import org.eclipse.b3.build.core.SharedScope;
+import org.eclipse.b3.build.operations.RunMainFunctionInModelOperation;
+import org.eclipse.b3.provisional.core.BuildException;
 import org.eclipse.b3.utils.BeeLangUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -25,142 +23,73 @@ import org.eclipse.xtext.ui.editor.outline.ContentOutlineNode;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
 
+@Deprecated
 public class ExecuteHandler extends AbstractHandler {
-	private boolean performResolve;
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		final MessageConsole b3Console = BeeLangConsoleUtils.getBeeLangConsole();
-		PrintStream b3ConsoleOutputStream = BeeLangConsoleUtils.getConsoleOutputStream(b3Console);
+		PrintStream b3out = BeeLangConsoleUtils.getConsoleOutputStream(b3Console);
+		PrintStream b3err = BeeLangConsoleUtils.getConsoleErrorStream(b3Console);
+
 		try {
-			b3ConsoleOutputStream.println("Running the main function...");
+			b3out.println("b3: Running function main()...");
+						
 			EvaluationContext ctx = (EvaluationContext) event.getApplicationContext();
 			List<ContentOutlineNode> nodes = (List<ContentOutlineNode>) ctx.getDefaultVariable();
 			ContentOutlineNode node = nodes.get(0);
-			Object result = node.getEObjectHandle().readOnly(new IUnitOfWork<Object, EObject>() {
+			
+			IStatus result = node.getEObjectHandle().readOnly(new IUnitOfWork<IStatus, EObject>() {
 				@Override
-				public Object exec(EObject state) throws Exception {
-					B3BuildEngine engine = new B3BuildEngine();
-					try {
-						engine.defineBeeModel((BeeModel) state);
-					}
-					catch(Throwable e) {
-						PrintStream b3ConsoleErrorStream = BeeLangConsoleUtils.getConsoleErrorStream(b3Console);
-						try {
-							e.printStackTrace();
-							b3ConsoleErrorStream.println("Loading failed with error: " +
-									e.getClass().getName().toString() + " : " + e.getMessage());
-							if(e.getCause() != null) {
-								b3ConsoleErrorStream.println("Caused by: " + e.getCause().getMessage());
-								return null;
-							}
-
-						}
-						finally {
-							b3ConsoleErrorStream.close();
-						}
-
-					}
-					// get the resolution scope (in case a resolution is to be performed)
-					//
-					SharedScope resolutionScope = null;
-					// If resolving, run a resolution
-					if(isPerformResolve()) {
-						resolutionScope = engine.getInjector().getInstance(B3BuildConstants.KEY_RESOLUTION_SCOPE);
-						resolutionScope.enter(); // !remember to call exit()
-
-						IStatus status = engine.resolveAllUnits();
-						if(!status.isOK()) {
-							PrintStream b3ConsoleErrorStream = BeeLangConsoleUtils.getConsoleErrorStream(b3Console);
-							try {
-								// TODO: Better error reporting on failed resolution
-								b3ConsoleErrorStream.println("Resolution Failed with non OK status :");
-								b3ConsoleErrorStream.println(status.toString());
-
-							}
-							finally {
-								b3ConsoleErrorStream.close();
-								// if(resolutionScope != null) // in case the error has to do with resolution scope
-								// resolutionScope.exit();
-								// resolutionScope = null;
-							}
-
-						}
-					}
-					// find a function called main (use the first found) and call it with a List<Object> argv
-					IFunction main = null;
-					for(IFunction f : ((BeeModel) state).getFunctions()) {
-						if("main".equals(f.getName())) {
-							main = f;
-						}
-					}
-
-					if(main == null)
-						return null;
-					final List<Object> argv = new ArrayList<Object>();
-					argv.add(engine);
-					try {
-						return engine.callFunction(
-							"main", new Object[] { argv }, new Type[] { List.class });
-					}
-					catch(B3BackendException exprException) {
-						exprException.printStackTrace();
-						BExpression expr = exprException.getExpression();
-						int lineNumber = BeeLangUtils.getLineNumber(expr);
-
-						PrintStream b3ConsoleErrorStream = BeeLangConsoleUtils.getConsoleErrorStream(b3Console);
-						try {
-							b3ConsoleErrorStream.println(exprException.getMessage());
-							b3ConsoleErrorStream.println("        at " + BeeLangUtils.closestNamedElement(expr) + "(" +
-									exprException.getLocationString() + ":" + lineNumber + ").");
-							if(exprException.getCause() != null) {
-								b3ConsoleErrorStream.println("Caused by: " +
-										((exprException.getCause().getMessage() == null)
-												? exprException.getCause().getClass().getName()
-												: exprException.getCause().getMessage()));
-							}
-
-						}
-						finally {
-							b3ConsoleErrorStream.close();
-						}
-
-					}
-					catch(Throwable e) {
-						// Just print some errors
-						e.printStackTrace();
-						PrintStream b3ConsoleErrorStream = BeeLangConsoleUtils.getConsoleErrorStream(b3Console);
-						try {
-							b3ConsoleErrorStream.println(e.getMessage());
-						}
-						finally {
-							b3ConsoleErrorStream.close();
-						}
-					}
-					finally {
-						if(resolutionScope != null)
-							resolutionScope.exit();
-						resolutionScope = null;
-					}
-					return null;
+				public IStatus exec(EObject state) throws Exception {
+					return new B3BuildEngine().run(new RunMainFunctionInModelOperation((BeeModel)state));
+				}});
+			
+			if(result.isOK()) {
+				b3out.print("b3: ok ");
+				if(result instanceof IResultStatus) {
+					b3out.print("result=> ");
+					b3out.print(((IResultStatus)result).getResult());
 				}
-			});
-			b3ConsoleOutputStream.println("Result = " + (result == null
-					? "null"
-					: result.toString()));
-			return null;
+				b3out.print("\n");
+				return null; // specified by handler API.
+			}
+			if(result.matches(IStatus.CANCEL)) {
+				// operation was cancelled, possibly because of an error, or interuption
+				// can also be a multi status with more detail... for now just print 'canceled'.
+				b3out.print("canceled.");
+				return null;
+			}
+			
+			// Only error and warning remain
+			Throwable e = result.getException();
+
+			// Useful when debugging
+			BuildException.deeplyPrint(e, System.err, true);
+
+			// Simple error reporting
+			b3err.println("b3 error: " + result.getMessage());
+			if(e == null) {
+				return null;
+			}
+			if(e instanceof B3BackendException) {
+					B3BackendException exprException = (B3BackendException)e;
+					BExpression expr = exprException.getExpression();
+					int lineNumber = BeeLangUtils.getLineNumber(expr);
+
+					b3err.println(exprException.getMessage());
+					b3err.println("        at " + BeeLangUtils.closestNamedElement(expr) + "(" +
+						exprException.getLocationString() + ":" + lineNumber + ").");
+					
+					}
+				
+			BuildException.deeplyPrint(e, b3err, false);
 		}
 		finally {
-			b3ConsoleOutputStream.close();
+			b3err.close();
+			b3out.close();
 		}
-	}
-
-	public boolean isPerformResolve() {
-		return performResolve;
-	}
-
-	public void setPerformResolve(boolean performResolve) {
-		this.performResolve = performResolve;
+		return null; // dictated by Handler API
 	}
 }
---
+++ com.cloudsmith.publish.ui/src/com/cloudsmith/publish/ui/commands/GenerateRepositoriesHandler.java
@@ -0,0 +1,7 @@
+package com.cloudsmith.publish.ui.commands;
+
+import org.eclipse.b3.build.ui.commands.RunMainFunctionInActiveEditorHandler;
+
+public class GenerateRepositoriesHandler extends RunMainFunctionInActiveEditorHandler {
+	// currently just runs main function....
+}
